666c43e19317a7e0a460a3cc34f7205d07b40b55
Mattea <mattea.sax@gmail.com>
2024-01-28 10:04:51 +0100

    minor changes

--- my-portfolio-page-vite/src/main.ts
+++ my-portfolio-page-vite/src/main.ts
@@ -5,9 +5,13 @@ const navbarUlEl = document.querySelector<HTMLElement>("#nav-bar-ul")!;
 // about section
 const aboutSectionEl = document.querySelector<HTMLElement>("#about-section")!;
 // contact section
-
 const contactSectionEl =
   document.querySelector<HTMLElement>("#contact-section")!;
+// contact button in about section
+const aboutSectionContactBtn =
+  document.querySelector<HTMLLinkElement>("#contact-button")!;
+
+// cv button in about section
 
 // create function to toggle popup-section windows
 // add eventlistener to navbar ul
@@ -20,5 +24,14 @@ navbarUlEl.addEventListener("click", (e: MouseEvent) => {
   } else if ((e.target as HTMLElement).innerText === "Contact") {
     contactSectionEl.classList.remove("hide");
     console.log("you clicked contact link");
+  }
+});
+
+// add eventlistener to contact button
+
+aboutSectionContactBtn.addEventListener("click", (e: MouseEvent) => {
+  if ((e.target as HTMLElement).innerText === "CONTACT ME") {
+    console.log("you want to contact me!");
+    contactSectionEl.classList.remove("hide");
   }
 });
