a918357a49facb7b75f1bea99bf3d313b209fce0
Mattea <mattea.sax@gmail.com>
2024-01-28 08:34:02 +0100

    added references to navbar links and edited event listener to remove class of hide when links are clicked

--- my-portfolio-page-vite/src/main.ts
+++ my-portfolio-page-vite/src/main.ts
@@ -1,9 +1,24 @@
 import "./style.css";
 
+// navbar
 const navbarUlEl = document.querySelector<HTMLElement>("#nav-bar-ul")!;
+// about section
+const aboutSectionEl = document.querySelector<HTMLElement>("#about-section")!;
+// contact section
+
+const contactSectionEl =
+  document.querySelector<HTMLElement>("#contact-section")!;
 
 // create function to toggle popup-section windows
+// add eventlistener to navbar ul
 
-navbarUlEl.addEventListener("click", () => {
+navbarUlEl.addEventListener("click", (e: MouseEvent) => {
   console.log("you clicked ul");
+  if ((e.target as HTMLElement).innerText === "About") {
+    aboutSectionEl.classList.remove("hide");
+    console.log("you clicked about link");
+  } else if ((e.target as HTMLElement).innerText === "Contact") {
+    contactSectionEl.classList.remove("hide");
+    console.log("you clicked contact link");
+  }
 });
