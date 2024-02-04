91709c7ce104e2bfa4d5403f734f3fca8adaf4fe
Mattea <mattea.sax@gmail.com>
2024-02-04 17:19:27 +0100

    added eventlistener to home-button in about section

--- my-portfolio-page-vite/src/main.ts
+++ my-portfolio-page-vite/src/main.ts
@@ -10,7 +10,9 @@ const contactSectionEl =
 // contact button in about section
 const aboutSectionContactBtn =
   document.querySelector<HTMLLinkElement>("#contact-button")!;
-
+// home link-button in about section
+const aboutSectionHomeBtn =
+  document.querySelector<HTMLButtonElement>("#about-home-btn");
 // cv button in about section
 
 // create function to toggle popup-section windows
@@ -33,5 +35,13 @@ aboutSectionContactBtn.addEventListener("click", (e: MouseEvent) => {
   if ((e.target as HTMLElement).innerText === "CONTACT ME") {
     console.log("you want to contact me!");
     contactSectionEl.classList.remove("hide");
+  }
+});
+
+// add eventlistener to home button
+
+aboutSectionHomeBtn?.addEventListener("click", (e: MouseEvent) => {
+  if ((e.target as HTMLElement).innerText === "Home") {
+    aboutSectionEl.classList.add("hide");
   }
 });
